069631de402e165a066e1ab86fed14ae8bb7908f
DartGys <108235020+DartGys@users.noreply.github.com>
2023-09-26 14:29:25 +0300

    Add project files.

---
+++ wwwroot/js/listDelete.js
@@ -0,0 +1,41 @@
+﻿// JavaScript-код для додавання завдань та видалення їх
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+    var taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        var taskItem = document.createElement("li");
+        taskItem.textContent = taskText;
+
+        // Додати кнопку "Видалити" до кожного завдання
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "button-delete-task"
+        deleteButton.onclick = function () {
+            taskList.removeChild(taskItem);
+            saveTasks(); // При видаленні завдання оновити Local Storage
+        };
+
+        taskItem.appendChild(deleteButton);
+
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+
+        saveTasks(); // При додаванні завдання оновити Local Storage
+    }
+}
+
+// JavaScript-код для очищення списку ToDo
+function clearTasks() {
+    var taskList = document.getElementById("taskList");
+    taskList.innerHTML = ""; // Видалимо всі дочірні елементи у списку
+    localStorage.removeItem("tasks"); // Видалимо дані з Local Storage
+}
+
+// Додайте кнопку або елемент у вашу сторінку, який викличе функцію clearTasks()
+var clearButton = document.createElement("button");
+clearButton.textContent = "Clear All Tasks";
+clearButton.className = "button-delete"
+clearButton.onclick = clearTasks;
+document.body.appendChild(clearButton); // Додайте кнопку на сторінку
---
+++ wwwroot/js/listSave.js
@@ -0,0 +1,50 @@
+﻿// JavaScript-код для збереження завдань у Local Storage
+function saveTasks()
+{
+    var taskList = document.getElementById("taskList");
+    var tasks = [];
+    for (var i = 0; i < taskList.children.length; i++)
+    {
+        tasks.push(taskList.children[i].textContent);
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// JavaScript-код для відновлення завдань з Local Storage
+function loadTasks() {
+    var taskList = document.getElementById("taskList");
+    var savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+        var tasks = JSON.parse(savedTasks);
+        for (var i = 0; i < tasks.length; i++) {
+            var taskItem = document.createElement("li");
+
+            // Видалити "Delete" текст, якщо він присутній у завданні
+            var taskText = tasks[i].replace("Delete", "");
+
+            taskItem.textContent = taskText;
+
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.className = "button-delete-task";
+            deleteButton.onclick = function (event) {
+                var listItem = event.target.parentElement;
+                taskList.removeChild(listItem);
+                saveTasks(); // При видаленні завдання оновити Local Storage
+            };
+
+            taskItem.appendChild(deleteButton);
+            taskList.appendChild(taskItem);
+        }
+    }
+}
+
+
+
+// Викликати функцію loadTasks() при завантаженні сторінки
+window.onload = loadTasks;
+
+
+// Зберегти завдання при додаванні або видаленні
+document.getElementById("taskInput").addEventListener("input", saveTasks);
+document.getElementById("taskList").addEventListener("input", saveTasks);
